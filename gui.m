f2863050885fc8f41d004dad8e8c0728286c96c6
Justin <justinpinkney@gmail.com>
2018-08-02 22:15:39 +0100

    Use java callback

--- gui.m
+++ gui.m
@@ -16,8 +16,9 @@ classdef gui < handle
             obj.Input = uicontrol('Parent', f, ...
                 'Style', 'edit', ...
                 'Units', 'normalized', ...
-                'Position', [0, 0, 1, 0.5], ...
-                'KeyPressFcn', @obj.update);
+                'Position', [0, 0, 1, 0.5]);
+            jInput = findjobj(obj.Input);
+            jInput.KeyTypedCallback = @obj.update;
             obj.Output = uicontrol('Parent', f, ...
                 'Style', 'listbox', ...
                 'Units', 'normalized', ...
@@ -26,13 +27,7 @@ classdef gui < handle
         end
         
         function update(obj, src, event)
-            if strcmp(event.Key, 'backspace')
-                if ~isempty(obj.Query)
-                    obj.Query = obj.Query(1:end-1);
-                end
-            else
-                obj.Query = [obj.Query, event.Character];
-            end
+            obj.Query = src.getText;
             result = fuzzyMatch(obj.Query, obj.Index)
             obj.Output.String = result.allText;
             
--- install.m
+++ install.m
@@ -1,3 +1,4 @@
 function install
     javaaddpath('java/commons-text-1.4.jar');
+    addpath('extern');
 end
